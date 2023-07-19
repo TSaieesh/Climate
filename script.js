74f675dc1f46536bbd61d3102a42abf29ffd4113
Tirumani Saieesh <109863921+TSaieesh@users.noreply.github.com>
2023-07-19 20:01:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function  morning(){
+    document.getElementById("climateImage").src="https://hips.hearstapps.com/hmg-prod/images/sunrise-quotes-21-1586892331.jpg?crop=1.00xw:0.895xh;0,0&resize=1200:*";
+}
+
+function afternoon(){
+    document.getElementById("climateImage").src="https://c1.wallpaperflare.com/preview/172/476/47/sunset-coconut-trees-afternoon-palm-trees.jpg";
+}
+
+function night(){
+    document.getElementById("climateImage").src="https://c4.wallpaperflare.com/wallpaper/36/398/507/night-palm-trees-wallpaper-preview.jpg";
+}
